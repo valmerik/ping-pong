c9159d7fc2e8cb8c8980e300688412bebdfa3efa
valmerik <136348950+valmerik@users.noreply.github.com>
2023-06-16 17:41:34 +0300

    Update ping-pong.py

--- ping-pong.py
+++ ping-pong.py
@@ -1,5 +1,6 @@
 from pygame import *
 from random import randint
+font.init()
 
 window = display.set_mode((700, 500))
 display.set_caption('Пинг-понг')
@@ -36,10 +37,17 @@ class Player(GameSprite):
 
 stick_l = Player('polosa2.png', 5, 630, 250)
 stick_r = Player('polosa2.png', 5, 10, 250)
+ball = GameSprite('krug3.png', 3, 350, 250)
+
+font = font.Font(None, 50)
+l_lose = font.render('ЛЕВЫЙ ИГРОК ПРОИГРАЛ!!!', True, (255, 255, 255))
+r_lose = font.render('ПРАВЫЙ ИГРОК ПРОИГРАЛ!!!', True, (255, 255, 255))
 
 clock = time.Clock()
 FPS = 60
 
+speed_x = 3
+speed_y = 3
 
 run = True
 finish = False
@@ -52,9 +60,27 @@ while run:
         window.blit(background,(0,0))
         stick_l.reset()
         stick_r.reset()
+        ball.reset()
 
         stick_l.update_l()
         stick_r.update_r()
+
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+        if sprite.collide_rect(stick_l, ball):
+            speed_x *= -1
+        if sprite.collide_rect(stick_r, ball):
+            speed_x *= -1
+        if ball.rect.y >= 435 or ball.rect.y <= 0:
+            speed_y *= -1
+        
+        if ball.rect.x <= 0:
+            window.blit(l_lose, (100,250))
+            finish = True
+        if ball.rect.x >= 635:
+            window.blit(r_lose, (100,250))
+            finish = True
+
 
     clock.tick(FPS)
     display.update()
